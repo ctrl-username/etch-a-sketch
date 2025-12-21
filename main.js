3c18567e7d110d9a117c1363463032cd87ef5f05
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-21 12:22:53 +0100

    feat(ui): create variables for refrencing and creating grid elements

---
+++ main.js
@@ -0,0 +1,10 @@
+//  create a 16 x 16 grid of square divs
+// use flexbox to make the divs appear as a grid
+// set up hover effect so grid change color when hovered
+// add button user can use to set grid size
+//
+//reference to grid container for adding grids
+const gridContainer = document.querySelector(".container");
+
+//create grid div element with class attribute grid
+const grids = document.createElement("div").setAttribute("class", "grid");
