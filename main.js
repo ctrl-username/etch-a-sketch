4a280c26645c91b8b39cb095abe5228806a6ecf8
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-24 21:51:48 +0100

    create grid function

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//  create a 16 x 16 grid of square divs
-// use flexbox to make the divs appear as a grid
+//  create a 16 x 16 grid of square divs // use flexbox to make the divs appear as a grid
 // set up hover effect so grid change color when hovered
 // add button user can use to set grid size
 //
@@ -7,4 +6,9 @@
 const gridContainer = document.querySelector(".container");
 
 //create grid div element with class attribute grid
-const grids = document.createElement("div").setAttribute("class", "grid");
+const grid = document.createElement("div");
+grid.setAttribute("class", "grid");
+
+// function for creating grid
+
+function createGrid(size) {}
