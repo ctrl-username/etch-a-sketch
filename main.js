300b19c52b8f9834c560e1064918852fbee1aa09
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-29 14:13:12 +0100

    make grid container width dynamic

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 //  create a 16 x 16 grid of square d:ivs // use flexbox to make the divs appear as a grid
 // set up hover effect so grid change color when hovered
 // add button user can use to set grid size
-//
+// grid container width
+const containerWidth =400 
 //reference to sketch pad main container 
 const gridBox = document.querySelector(".container");
 // create container for storing grids
 const gridContainer = document.createElement("div");
+// set grid container width and height
+gridContainer.style.width = `${containerWidth}px`
+
+gridContainer.style.height = `${containerWidth}px`
 //set grids container class to grid-container
 gridContainer.setAttribute("class", "grid-container")
 // place grids container inside main container
 gridBox.append(gridContainer)
 //create grid div element with class attribute grid
 
-const containerWidth =400 
+
 
 // function for creating grid
 function createGrid(size) {
